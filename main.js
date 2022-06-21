fd0d73c68e75e260db1d77706ce494b8178fdf40
LuyolG <1402098743@qq.com>
2022-06-21 20:38:50 +0800

    完成home首页的开发

--- main.js
+++ main.js
@@ -1,25 +1,43 @@
-
-// #ifndef VUE3
 import Vue from 'vue'
 import App from './App'
 
+// 封装网络请求-----------------------------
+import {
+	$http
+} from '@escook/request-miniprogram'
+// 请求挂载到 uni 身上，方便全局使用
+uni.$http = $http
+// 公共Url
+$http.baseUrl = 'https://api-ugo-web.itheima.net'
+// 设置请求前后时做的一些事情
+$http.beforeRequest = function(options) {
+	uni.showLoading({
+		title: '数据加载中...',
+	})
+}
+$http.afterRequest = function(options) {
+	uni.hideLoading()
+}
+
+
+//封装 uni.showToast------------------------
+uni.$showMsg=function(title="数据加载失败！",duration=1500){
+	uni.showToast({
+		title,
+		duration,
+		icon:'none'
+	})
+}
+
+
+// 关闭提示
 Vue.config.productionTip = false
+
+
 
 App.mpType = 'app'
 
 const app = new Vue({
-    ...App
+	...App
 })
 app.$mount()
-// #endif
-
-// #ifdef VUE3
-import { createSSRApp } from 'vue'
-import App from './App.vue'
-export function createApp() {
-  const app = createSSRApp(App)
-  return {
-    app
-  }
-}
-// #endif
